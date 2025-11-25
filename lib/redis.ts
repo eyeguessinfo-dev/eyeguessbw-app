9eb635f2e202ce7f6e7ea942222079a04528be33
Christopher Brannon <christopherbrannon@Christophers-MacBook-Pro.local>
2025-11-24 21:54:22 -0500

    Add Redis test API route

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from '@upstash/redis'
 
-if (!process.env.KV_REST_API_URL || !process.env.KV_REST_API_TOKEN) {
+if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
   throw new Error('Missing Redis environment variables')
 }
 
